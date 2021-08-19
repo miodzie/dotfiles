0e7be86773a9b0d4d1f15287d5c2033a93cdd66c
miodzie <miodzie@protonmail.com>
2021-08-19 18:19:05 -0400

    CYA NERD

--- config/nvim/lua/vulski/lsp.lua
+++ config/nvim/lua/vulski/lsp.lua
@@ -33,18 +33,6 @@ local on_attach = function(client, bufnr)
 
 end
 
--- -- Use a loop to conveniently call 'setup' on multiple servers and
--- -- map buffer local keybindings when the language server attaches
--- local servers = { 'pyright', 'rust_analyzer', 'tsserver' }
--- for _, lsp in ipairs(servers) do
---   nvim_lsp[lsp].setup {
---     on_attach = on_attach,
---     flags = {
---       debounce_text_changes = 150,
---     }
---   }
--- end
-
 nvim_lsp.tsserver.setup{ on_attach=on_attach }
 nvim_lsp.intelephense.setup{ on_attach=on_attach }
 nvim_lsp.gopls.setup{
--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-echo "Cloning Vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# TODO add vim-plug, literally everything else
 
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
