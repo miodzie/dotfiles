bf68d78fe37ed55f6347c05f4e0d6a96ccb10e9b
miodzie <miodzie@protonmail.com>
2021-11-29 09:47:43 -0500

    winners rewrite history

---
+++ scripts/commit-replace.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="your-old-email@example.com"
+CORRECT_NAME="miodzie"
+CORRECT_EMAIL="miodzie@protonmail.com"
+
+# if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+# then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+# fi
+# if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+# then
+#     export GIT_AUTHOR_NAME="$CORRECT_NAME"
+#     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+# fi
+' --tag-name-filter cat -- --branches --tags
+
+# then: git push --force --tags origin 'refs/heads/*'
+# or only selected references of the branches: git push --force --tags origin 'refs/heads/develop'
